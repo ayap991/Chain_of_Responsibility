3cafb98bd4008f8b7dd397f56ed080e806861d99
Paola Kolar <kolar.paola@gmail.com>
2020-06-16 00:09:36 +0200

    Create TextPreview

---
+++ TextPreviewer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chain_of_Responsibility
+{
+    public class TextPreviewer : Previewer
+    {
+        public override void HandleFileType(OpenFileDialog ofd, Form form)
+        {
+            string extension = Path.GetExtension(ofd.SafeFileName);
+            if (extension == ".txt")
+                CreateTextBox(ofd, form);
+            else if (NextPreviewer != null)
+                NextPreviewer.HandleFileType(ofd, form);
+        }
+
+        TextBox textBox;
+        private void CreateTextBox(OpenFileDialog ofd, Form form)
+        {
+            string fileContent="";
+            using (StreamReader reader = new StreamReader(ofd.OpenFile()))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            textBox = new TextBox();
+            textBox.Location = new System.Drawing.Point(12, 54);
+            textBox.Size = new System.Drawing.Size(617, 330);
+            textBox.ScrollBars = ScrollBars.Vertical;
+            textBox.Multiline = true;
+            form.Controls.Add(textBox);
+            textBox.Text = fileContent;
+
+        }
+    }
+}
