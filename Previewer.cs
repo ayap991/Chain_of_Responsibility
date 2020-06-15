494b9f835a8620a941871717f9671647c37f38d3
Paola Kolar <kolar.paola@gmail.com>
2020-06-15 22:46:51 +0200

    Create abstract class Previewer

---
+++ Previewer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chain_of_Responsibility
+{
+    abstract public class Previewer
+    {
+        protected Previewer NextPreviewer;
+        public void SetNextPreviewer(Previewer nextPreviewer)
+        {
+            this.NextPreviewer = nextPreviewer;
+        }
+        public abstract void HandleFileType(OpenFileDialog ofd, Form form);
+    }
+}
